e90fb600764bd340d9d3f3a3fe41d773f7ca5521
Brainy0789 <bpurdy12@icloud.com>
2025-07-15 16:28:20 -0700

    popups!

--- source/backend/SystemStuff.hx
+++ source/backend/SystemStuff.hx
@@ -1,20 +1,44 @@
 package backend;
+
 import openfl.Lib;
+import sys.io.Process;
 
 class SystemStuff {
-    //For now this stuff just uses trace()
+    public static function popup(title:String, message:String):Void {
+        #if windows
+        var script = 'Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show("' + message + '", "' + title + '")';
+        var p = new Process("powershell", ["-Command", script]);
+        p.close();
+        #elseif mac
+        var script = 'display dialog "' + message + '" with title "' + title + '" buttons {"OK"} default button 1';
+        var p = new Process("osascript", ["-e", script]);
+        p.close();
+        #elseif linux
+        var p = new Process("zenity", ["--info", "--title=" + title, "--text=" + message]);
+        p.close();
+        #else
+        trace(title + "\n" + message);
+        #end
+    }
 
-	public static function popup(title, message):Void
-	{
-		trace(title + "\n" + message);
-	}
+    public static function notification(title:String, message:String):Void {
+        #if windows
+        var script = '$app = New-Object -ComObject WScript.Shell; $app.Popup("' + message + '", 0, "' + title + '", 0)';
+        var p = new Process("powershell", ["-Command", script]);
+        p.close();
+        #elseif mac
+        var script = 'display notification "' + message + '" with title "' + title + '"';
+        var p = new Process("osascript", ["-e", script]);
+        p.close();
+        #elseif linux
+        var p = new Process("notify-send", [title, message]);
+        p.close();
+        #else
+        trace(title + "\n" + message);
+        #end
+    }
 
-	public static function notification(title, message):Void
-	{
-		trace(title + "\n" + message);
-	}
-
-    public static function crash(reallyCrash:Bool = false, showPopUp:Bool = false, title:String = "Title", message:String = "Message") {
+    public static function crash(reallyCrash:Bool = false, showPopUp:Bool = false, title:String = "Title", message:String = "Message"):Void {
         if (reallyCrash) {
             if (showPopUp) popup(title, message);
             Lib.application.window.close();
--- source/backend/UIData.hx
+++ source/backend/UIData.hx
@@ -4,44 +4,27 @@ import haxe.Json;
 import sys.io.File;
 import haxe.ds.StringMap;
 
+typedef UITextEntry = {
+	var x:Float;
+	var y:Float;
+	var centerX:Bool;
+	var centerY:Bool;
+	var scrollFactorX:Float;
+	var scrollFactorY:Float;
+	var text:String;
+	var tag:String;
+}
+
 typedef UIFile = {
-	var directory:String;
-	@:optional var isPixel:Null<Bool>;
-	@:optional var preload:Dynamic;
-	@:optional var objects:Array<Dynamic>;
-	@:optional var _editorMeta:Dynamic;
+	var noteSkin:String;
+    var healthBarImage:String;
+    var healthBarX:Float;
+    var healthBarY:Float;
+    var healthBarDownScrollY:Float;
+    var centerHealthBarX:Bool;
+    var centerHealthBarY:Bool;
+    var text:Array<UITextEntry>;
 }
 
 class UIData {
-	// Static storage for all loaded UI files
-	static var uiMap:StringMap<UIFile> = new StringMap();
-
-	/**
-	 * Load a UI JSON file from path and store it under a name
-	 */
-	public static function load(name:String, path:String):Void {
-		try {
-			var content = File.getContent(path);
-			var parsed:UIFile = Json.parse(content);
-			uiMap.set(name, parsed);
-		} catch (e) {
-			trace('Failed to load UI file "$name" at $path: $e');
-		}
-	}
-
-	/**
-	 * Get a UIFile by name
-	 */
-	public static function get(name:String):Null<UIFile> {
-		return uiMap.get(name);
-	}
-
-	/**
-	 * Preload multiple UI files if needed
-	 */
-	public static function preloadAll(files:Array<{name:String, path:String}>):Void {
-		for (file in files) {
-			load(file.name, file.path);
-		}
-	}
 }
