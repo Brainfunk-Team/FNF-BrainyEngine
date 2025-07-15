28b7c8d2a9eea6164ca9869386040b0008b1926f
Brainy0789 <bpurdy12@icloud.com>
2025-07-15 16:53:56 -0700

    Update SystemStuff.hx

--- source/backend/SystemStuff.hx
+++ source/backend/SystemStuff.hx
@@ -22,10 +22,12 @@ class SystemStuff {
     }
 
     public static function notification(title:String, message:String):Void {
-        #if windows
-        var script = '$app = New-Object -ComObject WScript.Shell; $app.Popup("' + message + '", 0, "' + title + '", 0)';
+       #if windows
+        var script = "$" + 'app = New-Object -ComObject WScript.Shell; $' + 'app.Popup("' + message + '", 0, "' + title + '", 0)';
         var p = new Process("powershell", ["-Command", script]);
         p.close();
+        #end
+
         #elseif mac
         var script = 'display notification "' + message + '" with title "' + title + '"';
         var p = new Process("osascript", ["-e", script]);
