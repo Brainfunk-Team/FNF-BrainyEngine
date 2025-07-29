5154b2759c05fa9cb2f422a4655e00721f74ead8
Brainy0789 <bpurdy12@icloud.com>
2025-07-28 17:34:30 -0700

    fixed something

--- source/states/HScriptState.hx
+++ source/states/HScriptState.hx
@@ -24,6 +24,7 @@ class HScriptState extends MusicBeatState
         var directories:Array<String> = Mods.directoriesWithFile("", "states/" + stateFile, true);
 
         state = new HScript(null, directories[0], null, false);
+        state.set("state", this);
 
         super();
     }
